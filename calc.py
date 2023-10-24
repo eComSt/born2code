cc054fb2708acf49a68a07f255c2ada0a4c4d0f2
eComS <eComS@live.com>
2023-10-24 22:47:48 +0700

    123

---
+++ calc.py
@@ -0,0 +1,15 @@
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+summ = number1 + number2
+diff = number1 - number2
+mulpt = number1 * number2
+div1 = number1 / number2
+div2 = number2 // number1
+div3 = number1 % number2
+print(summ)
+print(diff)
+print(mulpt)
+print(div1)
+print(div2)
+print(div3)
+print(type(diff))
---
+++ lesson1.py
@@ -0,0 +1,13 @@
+print('Hello world!')
+x = 0
+x = x + 1
+_ = 'Alex'
+print(_)
+# name = input("Как вас зовут? ")
+print("Привет ", name)
+x = '3.1415926'
+x = float(x)
+x = int(x)
+print(x)
+x= "42"
+x = int(x)
