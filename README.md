3de1f8664f7295f8a06d5ac4a5e7a7e17c465ae0
eComS <eComS@live.com>
2023-10-31 22:49:45 +0700

    123

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ telegram @eComStation
 discord: https://discord.gg/SXuWTC5D
 
 [https://discord.gg/SXuWTC5D](https://discord.gg/SXuWTC5D)
+
+![](https://lh7-us.googleusercontent.com/wO1k3_9vR1AuZevuPVvr9wqjuFZ55rChH_vgkBl9xtoffrA6JshxloF1bmpPsFriTJlQXUBtb6ztdS_5AlGdGK940ii-ygOaSMwxDZVMGfaH-6nLDJN6sLhFYrNnWLkqeJJOJj9BaKfXY4Ams2I70AF8RwNmnfQi=s2048)
--- lesson2/calc.py
+++ lesson2/calc.py
@@ -1,30 +1,26 @@
-number1 = int(input("Enter first number: "))
-number2 = int(input("Enter second number: "))
-oper = input("Enter operation: ")
-if oper == '+':
-    result = number1 + number2
-    print("Sum: " + str(result))
-elif oper == '-':
-    result = number1 - number2
-    print("Sub: ", str(result))
-elif oper == '*':
-    result = number1 * number2
-    print("Mul: " + str(result))
-elif oper == '/':
-    result = number1 / number2
-    print(f"Div: {result}")
-else:
-    print("Unknown operation")
-summ = number1 + number2
-diff = number1 - number2
-mulpt = number1 * number2
-div1 = number1 / number2
-div2 = number2 // number1
-div3 = number1 % number2
-print(summ)
-print(diff)
-print(mulpt)
-print(div1)
-print(div2)
-print(div3)
-print(type(diff))
+number1 = input("Enter first number: ")
+number2 = input("Enter second number: ")
+if number1.isnumeric() and number2.isnumeric():
+    number1 = int(number1)
+    number2 = int(number2)
+    oper = input("Enter operation: ")
+    if oper == '+':
+        result = number1 + number2
+        print("Sum: " + str(result))
+    elif oper == '-':
+        result = number1 - number2
+        print("Sub: ", str(result))
+    elif oper == '*':
+        result = number1 * number2
+        print("Mul: " + str(result))
+    elif oper == '/':
+        result = number1 / number2
+        print(f"Div: {result}")
+    elif oper == '//':
+        result = number1 // number2
+        print(f"Div int: {result}")
+    elif oper == '%':
+        result = number1 % number2
+        print("Mod: {result}")
+    else:
+        print("Unknown operation")
