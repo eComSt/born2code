b3fa07237c7947db265430c37515ffcf86477893
eComS <eComS@live.com>
2023-12-05 22:57:57 +0700

    123

--- mod_2/lesson1/new.py
+++ mod_2/lesson1/new.py
@@ -1,4 +1,12 @@
 a = {1,2,3}
-b = {2,3}
-print(b.issubset(a))
-print(a>=b)
+b = {2,3,4}
+print(b.union(a))
+print("union", a|b)
+print("inter",a&b)
+print(" diff",a-b)
+print("sym_dif",a^b)
+from random import randint
+lst = []
+for i in range(10):
+    lst.append(randint(1,100))
+print(lst)
