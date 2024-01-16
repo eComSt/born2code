3f0073634db2e5aecb2af6f734615a9cc1d7bdf3
eComS <eComS@live.com>
2024-01-16 22:54:30 +0700

    213

--- 123.py
+++ 123.py
@@ -1,5 +1,17 @@
-# def prrr(name):
-#     return f'Hello, world! {name}'
-prrr = lambda name: f'Hello, world! {name}'
-x = prrr('Jack')
-print(x)
+class SportCar:
+    mark = ""
+    color = ""
+    model = "" 
+    max_speed = ""
+cars = []
+for _ in range (3):
+      car = SportCar()
+      car.mark = input()
+      car.color = input()
+      car.model = input ()
+      car.max_speed = input (int())
+      cars.append(car)
+      print('-------------------')
+print('Машины в автопарке')
+for car in cars:
+    print(f'Марка: {car.mark}, цвет: {car.color}, модель: {car.model}, максимальная скорость: {car.max_speed}')
