af23b5cd6a2c9a61f03640e1c28eaf8c979aeca2
ecoms <ecoms>
2024-03-26 22:28:05 +0700

    123

--- mod_3/lesson_8/code.py
+++ mod_3/lesson_8/code.py
@@ -21,7 +21,7 @@ class MainWindow(CTk):
         self.checkbox_frame = MyCheckboxFrame(self, title='Выбери цвет', values=('Красный', 'Синий', 'Зеленый','Желтый','Фиолетово-черный'))
         self.checkbox_frame.grid(row=0, column=0,  padx=10, pady=10, sticky='nsew')
         # создаем и размещаем фрейм с радиокнопками
-        self.radiobutton_frame = MyRadiobuttonFrame(self, title='Выбери фигуру', values=('Круг', 'Квадрат'))
+        self.radiobutton_frame = MyRadiobuttonFrame(self, title='Выбери фигуру', values=('Круг', 'Квадрат','Ромбододекаэдр'))
         self.radiobutton_frame.grid(row=0, column=1,  padx=10, pady=10, sticky='nsew')
     def show_info(self):
         pass
@@ -37,18 +37,26 @@ class MyCheckboxFrame(CTkFrame):
             checkbox = CTkCheckBox(self, text=values[i])
             checkbox.grid(row=i + 1, column=0, padx=10, pady=10, sticky="w")
             self.checkboxes.append(checkbox)
-        def get(self):
-            checked_checkboxes = []
-            for checkbox in self.checkboxes:
-                if checkbox.get() == 1:
-                    checked_checkboxes.append(checkbox.cget("text"))
-            return ', '.join(checked_checkboxes) 
 
+    def get(self):
+        checked_checkboxes = []
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
+        return ', '.join(checked_checkboxes) 
+    
 class MyRadiobuttonFrame(CTkFrame):
     def __init__(self, master, title, values):
         super().__init__(master)
+        self.grid_columnconfigure(0, weight=1)
+        self.figure = StringVar()
+        self.title = CTkLabel(self, text=title, fg_color="gray30", corner_radius=6)
+        self.title.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
+        for i in range(len(values)):
+            radiobutton = CTkRadioButton(self, text=values[i], value=values[i], variable=self.figure)
+            radiobutton.grid(row=i + 1, column=0, padx=10, pady=10, sticky="w")
     def get(self):
-        pass
+        return self.figure.get()
 
 root = MainWindow()
 root.mainloop()
