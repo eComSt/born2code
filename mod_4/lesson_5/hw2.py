b1dae9551a07fe101b108de7b43440eb86e411eb
ecoms <ecoms>
2024-05-14 21:31:26 +0700

    123

--- mod_4/lesson_5/hw2.py
+++ mod_4/lesson_5/hw2.py
@@ -1,10 +1,12 @@
-import telebot
-import configparser
 import random
-config = configparser.ConfigParser()
-config.read('config.ini')
-token = "7018542469:AAF8eACXN9IoJO56ae7Rd9HjRiVSRw5mMQ8"
-bot = telebot.TeleBot(token)
+import telebot
+bot = telebot.TeleBot("7018542469:AAF8eACXN9IoJO56ae7Rd9HjRiVSRw5mMQ8")
+
+facts = [
+    "Хамелеоны могут двигать глазами в разных направлениях одновременно.",
+    "Электрический угорь может вырабатывать энергию, способную зажечь 12 лампочек.",
+    "Морские котики могут задерживать дыхание на два часа.",
+]
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -14,8 +16,6 @@ def start(message):
     button3 = telebot.types.KeyboardButton('Расскажи интересный факт')
     kbrd.add(button1, button2, button3)
     bot.send_message(message.chat.id, "Добрый день! Я небольшой эхо бот", reply_markup=kbrd)
-
-
 
 @bot.message_handler(commands=['help'])
 def start(message):
@@ -27,22 +27,14 @@ def start(message):
 
 @bot.message_handler(content_types=['text'])
 def echo_message(message):
-    facts = [
-    "Хамелеоны могут двигать глазами в разных направлениях одновременно.",
-    "Электрический угорь может вырабатывать энергию, способную зажечь 12 лампочек.",
-    "Морские котики могут задерживать дыхание на два часа."
-    ]
-    print(message.text)
     if message.text == 'Привет':
-        text = "Привет, " + message.from_user.first_name + "!"
-    elif message.text == 'Как дела?':
-        text = "Не плохо, а у вас?"
+        text = "И тебе привет!"
     elif message.text == 'Расскажи интересный факт':
         text = random.choice(facts)
+    elif message.text == 'Как дела?':
+        text = 'Хорошо, а у вас?'
     else:
         text = message.text
     bot.reply_to(message, text)
 
-
-
-bot.polling()
+bot.polling(none_stop=True)
