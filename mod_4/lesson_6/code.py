895e0defd8608c5ee43daebba838fa852714ab9f
ecoms <ecoms>
2024-05-21 20:07:49 +0700

    123

--- mod_4/lesson_6/code.py
+++ mod_4/lesson_6/code.py
@@ -1,7 +1,9 @@
 import requests # подключаем библиотеку для отправки GET-запросов
+from pprint import pprint
+from datetime import datetime
 # координаты населенного пункта в формате "широта, долгота"
-coords = (59.9386, 30.3141)
-API_KEY = 'dc610ddd2e0bfa14d7ae1e16c57b389e' # ВАШ ключ с доступом к OpenWeather API
+coords = (55.041111, 82.934441)
+API_KEY = "a8f6fef7a296bbce70aa16aca85698e3" # ВАШ ключ с доступом к OpenWeather API
 # начальная часть GET-запроса к OpenWeather API
 url = 'https://api.openweathermap.org/data/2.5/weather?'
 params = {
@@ -12,3 +14,17 @@ params = {
     'lang': 'ru'
 }
 response = requests.get(url, params=params) 
+data = response.json()
+# pprint(data)
+main = data['main']
+description = data['weather'][0]['description']
+wind = data['wind']
+sunrise = data['sys']['sunrise']
+sunset = data['sys']['sunset']
+print(f"время: {(sunset-sunrise)//3600} часов {((sunset-sunrise)%3600)//60} минут")
+print(f"Восход в {datetime.fromtimestamp(sunrise).strftime('%H:%M:%S')}")
+print(f"Закат в {datetime.fromtimestamp(sunset).strftime('%H:%M:%S')}")
+print(f"Температура {main['temp']}°C, {description}")
+print(f"Ощущается как {main['feels_like']}°C")
+print(f"Ветер {wind['speed']} м/c")
+print(f"Влажность {main['humidity']}%")
